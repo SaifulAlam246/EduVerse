41d8e6087a563a168bdd00b71513a682e286aa73
saifulalam <saifulalamsaimon85@gmail.com>
2026-02-11 14:38:02 +0600

    supabase added

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from courses.views import CourseViewSet, AdminDashboardStats
 
 router = DefaultRouter()
+router.include_format_suffixes = False
 router.register('courses', CourseViewSet, basename='course')
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('admin-stats/', AdminDashboardStats.as_view(), name='admin-stats'),
+     path('', include(router.urls)),
 ]
--- edu_verse/urls.py
+++ edu_verse/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import api_root_view
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
